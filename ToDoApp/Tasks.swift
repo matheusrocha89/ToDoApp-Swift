2cd6dfc3b2e563746d4feb5ab5aa4d05dc124985
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-09-21 12:13:28 -0300

    Generating NSManagedObject Subclasses
    Tasks class form Model

---
+++ ToDoApp/Tasks.swift
@@ -0,0 +1,16 @@
+//
+//  Tasks.swift
+//  ToDoApp
+//
+//  Created by Matheus on 21/09/15.
+//  Copyright © 2015 Matheus Cruz Rocha. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Tasks: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
