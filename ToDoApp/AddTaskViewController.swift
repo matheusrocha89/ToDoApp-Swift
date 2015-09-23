1c4d46cf8305efa942c14b743b955c761e9d3395
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-09-22 21:35:54 -0300

    Creating saving new task behavior

--- ToDoApp/AddTaskViewController.swift
+++ ToDoApp/AddTaskViewController.swift
@@ -21,7 +21,6 @@ class AddTaskViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     // Do any additional setup after loading the view.
   }
   
@@ -30,14 +29,21 @@ class AddTaskViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  /*
+  func saveTask() -> Tasks {
+    let task = NSEntityDescription.insertNewObjectForEntityForName("Tasks", inManagedObjectContext: self.managedObjectContext) as! Tasks
+    task.title = self.taskTitle.text
+    task.completed = 0
+    self.saveContext()
+    return task
+  }
+  
   // MARK: - Navigation
   
   // In a storyboard-based application, you will often want to do a little preparation before navigation
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-  // Get the new view controller using segue.destinationViewController.
-  // Pass the selected object to the new view controller.
+    if !(sender!.isEqual(self.saveButton)) { return }
+    if !(self.taskTitle.text!.isEmpty) {
+      self.task = self.saveTask()
+    }
   }
-  */
-  
 }
